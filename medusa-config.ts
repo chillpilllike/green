e8e656004c289dfa83d6c3671b824ed23ac09985
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-11-20 12:18:53 +0530

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -6,7 +6,8 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd());
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
-    redisUrl: process.env.REDIS_URL, // Added redisUrl here
+    redisUrl: process.env.REDIS_URL, // Added redisUrl
+    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server", // Added workerMode
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
@@ -14,6 +15,10 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
+  },
+  admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true", // Added admin.disable
+    backendUrl: process.env.MEDUSA_BACKEND_URL, // Added admin.backendUrl
   },
   modules: [
     {
